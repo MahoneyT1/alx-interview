3f146d2bf1884cc2604f0cff692167544b452c0e
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-02 09:23:48 +0100

    solved pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
+
 """ Create a function def pascal_triangle(n):
 that returns a list of lists of integers
 representing the Pascal’s triangle of n
 """
 
 
-def pascal_triangle(n):
+def pascal_triangle(number):
     """ n is the length of the row
     """
-    triangle = []
-
-    if n <= 0:
+    if number <= 0:
         return []
 
-    for count in range(n):
-        row = [1] * (count + 1)
+    tri = []
 
-        for column in range(1, count):
-            row[column] = triangle[count - 1][column - 1] + \
-            triangle[count - 1][column]
+    for i in range(number):
+        # the first leading cell must start with 1
+        row = [1] * (i + 1)
 
-        triangle.append(row)
+        for k in range(1, i):
+            # the next row number is based on this
+            # adding the two number above
+            row[k] = tri[i - 1][k - 1] + tri[i - 1][k]
 
-    return triangle
+        tri.append(row)
+
+    return tri
+
+
